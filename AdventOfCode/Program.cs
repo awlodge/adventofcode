e70ae27c50306cbc1a05b9a94bf38d65b3b8c616
Alex Lodge <awlodge@gmail.com>
2024-12-01 15:02:01 +0000

    Add runner helper method

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,19 +1,5 @@
-﻿using AdventOfCode._2024;
-using System.Diagnostics;
+﻿using AdventOfCode;
+using AdventOfCode._2024;
 
-Console.WriteLine("========================");
-Console.WriteLine("Executing Day 1 (part 1)");
-var sw = Stopwatch.StartNew();
-var output = Day01.RunPart1();
-sw.Stop();
-Console.WriteLine(output);
-Console.WriteLine($"Ran in {sw.Elapsed.TotalSeconds} seconds");
-
-Console.WriteLine("========================");
-Console.WriteLine("Executing Day 1 (part 2)");
-sw = Stopwatch.StartNew();
-output = Day01.RunPart2();
-sw.Stop();
-Console.WriteLine(output);
-Console.WriteLine($"Ran in {sw.Elapsed.TotalSeconds} seconds");
-
+RunnerHelpers.Execute(Day01.RunPart1, "Day 1 (part 1)");
+RunnerHelpers.Execute(Day01.RunPart2, "Day 1 (part 2)");
---
+++ AdventOfCode/RunnerHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode;
+
+internal static class RunnerHelpers
+{
+    public static void Execute<T>(Func<T> action, string title)
+    {
+        Console.WriteLine("============================");
+        Console.WriteLine($"Executing {title}");
+        var sw = Stopwatch.StartNew();
+        T output = action();
+        sw.Stop();
+        Console.WriteLine($"Output: {output}");
+        Console.WriteLine($"[Ran in {sw.Elapsed.TotalSeconds} seconds]");
+        Console.WriteLine();
+    }
+}
