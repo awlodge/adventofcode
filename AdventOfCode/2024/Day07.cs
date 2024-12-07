0ac5cae3a47f6d607293b677c20aed4119b78b76
Alex Lodge <awlodge@gmail.com>
2024-12-07 08:27:40 +0000

    Day 7 (part 1)

---
+++ AdventOfCode/2024/Day07.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode.Helpers;
+
+namespace AdventOfCode._2024;
+
+public static class Day07
+{
+    private static readonly string InputPath = Path.Combine(Environment.CurrentDirectory, "2024/inputs/day07.txt");
+
+    public static long RunPart1()
+    {
+        return ParseFile(InputPath).TotalCalibrationResult();
+    }
+
+    public static long TotalCalibrationResult(string input)
+    {
+        return Parse(input).TotalCalibrationResult();
+    }
+
+    private static long TotalCalibrationResult(this IEnumerable<Equation> equations) =>
+        equations
+            .Where(eq => eq.IsValid())
+            .Sum(eq => eq.Result);
+
+    public static bool IsValid(this Equation equation) =>
+        CheckEquation(equation.Result, equation.Inputs);
+
+    private static bool CheckEquation(long result, List<int> inputs)
+    {
+        if (inputs.Count == 1)
+        {
+            return result == inputs[0];
+        }
+
+        if (result < inputs[^1])
+        {
+            return false;
+        }
+
+        if (CheckEquation(result - inputs[^1], inputs[..^1]))
+        {
+            return true;
+        }
+
+        if (result % inputs[^1] == 0 && CheckEquation(result / inputs[^1], inputs[..^1]))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static IEnumerable<Equation> ParseFile(string path)
+    {
+        return File.ReadLines(path).Select(l => ParseLine(l));
+    }
+
+    public static IEnumerable<Equation> Parse(string input)
+    {
+        return input.SplitLines().Select(l => ParseLine(l));
+    }
+
+    private static Equation ParseLine(string line)
+    {
+        var parts = line.Split(':', options: StringSplitOptions.TrimEntries);
+        return new Equation(Int64.Parse(parts[0]), parts[1].ParseAsInts().ToList());
+    }
+}
+
+public record Equation(long Result, List<int> Inputs);
---
+++ AdventOfCodeTests/2024/Day07Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode._2024;
+using FluentAssertions;
+
+namespace AdventOfCodeTests._2024;
+
+public class Day07Tests
+{
+    private const string TestInput = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+    public static IEnumerable<object[]> GetTestInputForCheckEquation()
+    {
+        HashSet<long> validResults = [190, 3267, 292];
+
+        foreach (var eq in Day07.Parse(TestInput))
+        {
+            yield return new object[] { eq, validResults.Contains(eq.Result) };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTestInputForCheckEquation))]
+    public void TestCheckEquation(Equation equation, bool expectedResult)
+    {
+        equation.IsValid().Should().Be(expectedResult);
+    }
+
+    [Fact]
+    public void TestTotalCalibrationResult()
+    {
+        Day07.TotalCalibrationResult(TestInput).Should().Be(3749);
+    }
+
+    [Fact]
+    public void TestDay7Part1()
+    {
+        Day07.RunPart1().Should().Be(12553187650171);
+    }
+}
