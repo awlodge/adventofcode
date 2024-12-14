0f1c01db36d6c9456722a2730b5b37bbaf0eedf1
Alex Lodge <awlodge@gmail.com>
2024-12-14 10:44:23 +0000

    Day 14 (part 2) - urgh

--- AdventOfCode/2024/Day14.cs
+++ AdventOfCode/2024/Day14.cs
@@ -2,12 +2,14 @@
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode._2024;
+using Map = Grid<char>;
 
 public static class Day14
 {
     private static readonly string InputPath = Path.Combine(Environment.CurrentDirectory, "2024/inputs/day14.txt");
-    private static readonly MapSize Map = new(101, 103);
+    private static readonly Map Map = Map.Generate('.', 101, 103);
     private const int Time = 100;
+    private static readonly bool Debug = false;
 
     [AdventOfCode2024(14, 1)]
     public static long RunPart1()
@@ -15,25 +17,68 @@ public static class Day14
         return ParseFile(InputPath).GetSafetyFactor(Map, Time);
     }
 
+    [AdventOfCode2024(14, 2)]
+    public static long RunPart2()
+    {
+        return ParseFile(InputPath).FindChristmasTree(Map);
+    }
+
     public static int GetSafetyFactor(string input, int mapWidth, int mapHeight, int time)
     {
-        return Parse(input).GetSafetyFactor(new MapSize(mapWidth, mapHeight), time);
+        return Parse(input).GetSafetyFactor(Map.Generate('.', mapWidth, mapHeight), time);
+    }
+
+    private static int FindChristmasTree(this IEnumerable<Robot> robots, Map map)
+    {
+        robots = robots.ToList();
+        int i = 0;
+        while (true)
+        {
+            if (robots.HasChristmasTree())
+            {
+                if (Debug)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Found christmas tree at {i}");
+                    robots.Print(map);
+                }
+                return i;
+            }
+            else
+            {
+                if (Debug)
+                {
+                    if (i % 100 == 1)
+                    {
+                        Console.Write(".");
+                    }
+                    if (i % 1000 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            i++;
+            robots.AsParallel().ForAll(r => r.Move(Map, 1));
+        }
     }
 
     public static string MoveRobot(string input, int mapWidth, int mapHeight, int time)
     {
-        return Robot.Parse(input).Move(new MapSize(mapWidth, mapHeight), time).ToString();
+        return Robot.Parse(input).Move(Map.Generate('.', mapWidth, mapHeight), time).ToString();
     }
 
-    private static Robot Move(this Robot robot, MapSize map, int time)
+    private static Robot Move(this Robot robot, Map map, int time)
     {
-        var delta = robot.Velocity * time;
-        Point newPos = new(PosMod(robot.Position.X + delta.X, map.Width),
-            PosMod(robot.Position.Y + delta.Y, map.Height));
-        return robot with { Position = newPos };
+        var deltaX = robot.Velocity.X * time;
+        var deltaY = robot.Velocity.Y * time;
+        Point newPos = new(PosMod(robot.Position.X + deltaX, map.Width),
+            PosMod(robot.Position.Y + deltaY, map.Height));
+        robot.Position = newPos;
+        return robot;
     }
 
-    private static int GetSafetyFactor(this IEnumerable<Robot> robots, MapSize map, int time)
+    private static int GetSafetyFactor(this IEnumerable<Robot> robots, Map map, int time)
     {
         int[] counts = [0, 0, 0, 0];
         robots
@@ -51,7 +96,23 @@ public static class Day14
         return counts.Aggregate(1, (acc, val) => acc * val);
     }
 
-    private static int GetQuadrant(this MapSize map, Point p)
+    private static bool HasChristmasTree(this IEnumerable<Robot> robots)
+    {
+        var robotPositions = robots.Select(r => r.Position).ToHashSet();
+        return robotPositions.Any(p => (
+            IsPoint(p)
+            && IsPoint(p + Directions.SouthWest)
+            && IsPoint(p + Directions.SouthEast)
+        ));
+
+        bool IsPoint(Point p) => (
+            robotPositions.Contains(p + Directions.SouthWest)
+            && robotPositions.Contains(p + Directions.South)
+            && robotPositions.Contains(p + Directions.SouthEast)
+            );
+    }
+
+    private static int GetQuadrant(this Map map, Point p)
     {
         var midWidth = map.Width / 2;
         var midHeight = map.Height / 2;
@@ -77,6 +138,14 @@ public static class Day14
         return r < 0 ? r + mod : r;
     }
 
+    private static void Print(this IEnumerable<Robot> robots, Map map)
+    {
+        map.Print(robots
+            .Select(r => r.Position)
+            .Distinct()
+            .ToDictionary(p => p, p => 'X'));
+    }
+
     private static IEnumerable<Robot> ParseFile(string path)
         => ParseInner(File.ReadLines(path));
 
@@ -89,6 +158,10 @@ public static class Day14
 
 internal partial record Robot(Point Position, Point Velocity)
 {
+    public Point Position { get; set; } = Position;
+
+    public Point Velocity { get; init; } = Velocity;
+
     [GeneratedRegex("p=(\\d+),(\\d+) v=([-\\d]+),([-\\d]+)")]
     private static partial Regex RobotRegex();
 
@@ -104,5 +177,3 @@ internal partial record Robot(Point Position, Point Velocity)
                 Int32.Parse(match.Groups[4].Value)));
     }
 }
-
-internal record MapSize(int Width, int Height);
