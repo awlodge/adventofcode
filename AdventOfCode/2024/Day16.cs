7a46e7b1f22ea2eebc3282a472cf412a47ff88e8
Alex Lodge <awlodge@gmail.com>
2024-12-18 07:29:10 +0000

    Add generic graph and shortest path implementation

--- AdventOfCode/2024/Day16.cs
+++ AdventOfCode/2024/Day16.cs
@@ -29,40 +29,25 @@ public static class Day16
 
     private static int GetLowestScore(this Map map)
     {
-        var unvisited = map.GetAllNodes()
-            .ToDictionary(n => n, n => IsStart(n) ? 0 : -1);
-
-        while (true)
+        var graph = new Graph<DirectedPoint>();
+        var allNodes = map.GetAllNodes().ToHashSet();
+        foreach (var node in allNodes)
         {
-            var node = unvisited
-                .Where(x => x.Value != -1)
-                .MinBy(x => x.Value)
-                .Key ?? throw new InvalidOperationException("No nodes left");
-
-            if (map.Lookup(node.Position) == 'E')
-            {
-                return unvisited[node];
-            }
-
-            foreach (var (n, e) in GetNeighbors(node))
-            {
-                var newScore = unvisited[node] + e;
-                if (unvisited[n] == -1 || newScore < unvisited[n])
-                {
-                    unvisited[n] = newScore;
-                }
-            }
-
-            unvisited.Remove(node);
+            graph.AddNode(node, GetNeighbors(node));
         }
 
-        bool IsStart(DirectedPoint n) =>
-            map.Lookup(n.Position) == 'S' && n.Direction == Directions.East;
+        var start = new DirectedPoint(map.Search(x => x == 'S').First(), Directions.East);
+        if (!allNodes.Contains(start))
+        {
+            throw new InvalidOperationException($"Can't find {start}");
+        }
+        return graph.ShortestPath(n => map.Lookup(n.Position) == 'S' && n.Direction == Directions.East,
+            n => map.Lookup(n.Position) == 'E');
 
         IEnumerable<(DirectedPoint, int)> GetNeighbors(DirectedPoint n)
         {
             var forward = new DirectedPoint(n.Position + n.Direction, n.Direction);
-            if (unvisited.ContainsKey(forward))
+            if (allNodes.Contains(forward))
             {
                 yield return (forward, 1);
             }
@@ -70,96 +55,13 @@ public static class Day16
             foreach (var q in n.Direction.Rotate())
             {
                 var candidate = new DirectedPoint(n.Position + q, q);
-                if (unvisited.ContainsKey(candidate))
+                if (allNodes.Contains(candidate))
                 {
                     yield return (candidate, 1001);
                 }
             }
         }
     }
-
-    private static int GetLowestScoreOld(this Map map, Point start)
-    {
-        Dictionary<Point, Dictionary<Point, int>> visited = [];
-
-        return GetLowestScoreInner(start, Directions.East, []);
-
-        int GetLowestScoreInner(Point p, Point dir, HashSet<Point> route)
-        {
-            if (route.Contains(p))
-            {
-                return -1;
-            }
-
-            if (VisitedTryLookup(p, dir, out var prevScore))
-            {
-                return (int)prevScore!;
-            }
-
-            if (map.Lookup(p) == 'E')
-            {
-                VisitedSetValue(p, dir, 0);
-                return 0;
-            }
-
-            List<int> scores = [];
-            var newRoute = new HashSet<Point>(route)
-            {
-                p
-            };
-            var forward = p + dir;
-            if (!map.IsWall(forward))
-            {
-                var score = GetLowestScoreInner(forward, dir, newRoute);
-                if (score != -1)
-                {
-                    scores.Add(1 + score);
-                }
-            }
-
-            foreach (var q in dir.Rotate())
-            {
-                var next = p + q;
-                if (!map.IsWall(next))
-                {
-                    var score = GetLowestScoreInner(next, q, newRoute);
-                    if (score != -1)
-                    {
-                        scores.Add(1001 + score);
-                    }
-                }
-            }
-
-            var result = scores.Count == 0 ? -1 : scores.Min();
-            VisitedSetValue(p, dir, result);
-            return result;
-        }
-
-        bool VisitedTryLookup(Point p, Point d, out int? s)
-        {
-            s = null;
-            if (visited.TryGetValue(p, out var dirs) && dirs.TryGetValue(d, out var res))
-            {
-                s = res;
-                return true;
-            }
-            return false;
-        }
-
-        void VisitedSetValue(Point p, Point d, int s)
-        {
-            if (visited.TryGetValue(p, out var dirs))
-            {
-                dirs[d] = s;
-            }
-            else
-            {
-                visited[p] = [];
-                visited[p][d] = s;
-            }
-        }
-    }
-
     private static bool IsWall(this Map map, Point p) =>
         map.Lookup(p) == '#';
 
@@ -182,4 +84,15 @@ public static class Day16
     }
 }
 
-internal record DirectedPoint(Point Position, Point Direction);
+internal sealed record DirectedPoint(Point Position, Point Direction)
+{
+    public bool Equals(DirectedPoint? other)
+    {
+        return (other != null && other.Position == Position && other.Direction == Direction);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Position.GetHashCode(), Direction.GetHashCode());
+    }
+}
---
+++ AdventOfCode/Helpers/Graph.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode.Helpers;
+
+internal class Graph<T> where T : class
+{
+    public Dictionary<T, Node<T>> Nodes { get; private set; } = [];
+
+    public bool AddNode(T v, IEnumerable<(T, int)> neighbors)
+    {
+        bool added = false;
+        if (!Nodes.TryGetValue(v, out var node))
+        {
+            added = true;
+            node = new Node<T>(v);
+            Nodes[node.Value] = node;
+        }
+
+        foreach ((T n, int e) in neighbors)
+        {
+            if (Nodes.TryGetValue(n, out var neighbor))
+            {
+                node.AddEdge(neighbor, e);
+            }
+            else
+            {
+                neighbor = new Node<T>(n);
+                node.AddEdge(neighbor, e);
+            }
+        }
+        return added;
+    }
+
+    public bool Contains(T v) => Nodes.ContainsKey(v);
+
+    public int ShortestPath(T start, T end) => ShortestPath(n => n == start, n => n == end);
+
+    public int ShortestPath(Func<T, bool> isStart, Func<T, bool> isEnd)
+    {
+        var unvisited = Nodes.Keys.ToDictionary(n => n, n => isStart(n) ? 0 : -1);
+
+        while (unvisited.Count > 0)
+        {
+            var node = unvisited
+                .Where(x => x.Value != -1)
+                .MinBy(x => x.Value)
+                .Key ?? throw new InvalidOperationException("No nodes left");
+
+            if (isEnd(node))
+            {
+                return unvisited[node];
+            }
+
+            foreach (var edge in Nodes[node].Edges)
+            {
+                if (unvisited.ContainsKey(edge.Neighbor.Value))
+                {
+                    var newScore = unvisited[node] + edge.Cost;
+                    if (unvisited[edge.Neighbor.Value] == -1 || newScore < unvisited[edge.Neighbor.Value])
+                    {
+                        unvisited[edge.Neighbor.Value] = newScore;
+                    }
+                }
+            }
+
+            unvisited.Remove(node);
+        }
+
+        throw new InvalidOperationException($"No path from start to end");
+    }
+}
+
+internal record Node<T>(T Value)
+{
+    public T Value { get; init; } = Value;
+
+    public List<Edge<T>> Edges { get; } = [];
+
+    public void AddEdge(Node<T> node, int weight)
+    {
+        Edges.Add(new Edge<T>(node, weight));
+    }
+}
+
+
+internal record Edge<T>(Node<T> Neighbor, int Cost);
