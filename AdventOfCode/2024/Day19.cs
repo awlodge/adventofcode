20cf17ef4c2f93a2e0fdbc5651cb29c2e826ed8f
Alex Lodge <alex.lodge@checkout.com>
2024-12-20 18:04:34 +0000

    Day 19 (part 2) - too slow

--- AdventOfCode/2024/Day19.cs
+++ AdventOfCode/2024/Day19.cs
@@ -6,27 +6,53 @@ public class Day19
 {
     private static readonly string InputPath = Path.Combine(Environment.CurrentDirectory, "2024/inputs/day19.txt");
 
-    private readonly HashSet<string> _possiblePatterns;
+    private readonly Dictionary<string, bool> _viablePatternCache = [];
+    private readonly Dictionary<string, HashSet<string>> _patternOptionCache = [];
+    private readonly HashSet<string> _patterns = [];
     private readonly int _minLength;
     private readonly int _maxLength;
 
     [AdventOfCode2024(19, 1)]
     public static int RunPart1()
     {
-        (var startPatterns, var targetPatterns) = ParseFile(InputPath);
+        var (startPatterns, targetPatterns) = ParseFile(InputPath);
         return CountViablePatterns(startPatterns, targetPatterns);
+    }
+    
+    [AdventOfCode2024(19, 2)]
+    public static int RunPart2()
+    {
+        var (startPatterns, targetPatterns) = ParseFile(InputPath);
+        return CountTotalPatternOptions(startPatterns, targetPatterns);
     }
 
     public Day19(IEnumerable<string> patterns)
     {
-        _minLength = patterns.Select(s => s.Length).Min();
-        _maxLength = patterns.Select(s => s.Length).Max();
-        _possiblePatterns = new HashSet<string>(patterns);
+        var possiblePatterns = patterns.ToHashSet();
+        var lengths = possiblePatterns.Select(p => p.Length).ToList();
+        _minLength = lengths.Min();
+        _maxLength = lengths.Max();
+        foreach (var pattern in possiblePatterns)
+        {
+            _patterns.Add(pattern);
+            _viablePatternCache[pattern] = true;
+            _patternOptionCache[pattern] = FindPatternOptions(pattern).Union([pattern]).ToHashSet();
+        }
     }
 
     public int CountViablePatterns(IEnumerable<string> patterns)
     {
-        return patterns.Count(p => IsViablePattern(p));
+        return patterns.Count(IsViablePattern);
+    }
+
+    public int TotalPatternOptions(IEnumerable<string> patterns)
+    {
+        return patterns.Sum(s =>
+        {
+            var c = FindPatternOptions(s).Count;
+            Console.Write(".");
+            return c;
+        });
     }
 
     public bool IsViablePattern(string pattern)
@@ -36,28 +62,52 @@ public class Day19
             return true;
         }
 
-        if (_possiblePatterns.Contains(pattern))
+        if (_viablePatternCache.TryGetValue(pattern, out var viablePattern))
         {
+            return viablePattern;
+        }
+
+        if (PossibleSubPatternLengths(pattern)
+            .Select(l => (pattern[..l], pattern[l..]))
+            .Any(x => IsViablePattern(x.Item1) && IsViablePattern(x.Item2)))
+        {
+            _viablePatternCache[pattern] = true;
             return true;
         }
+        
+        _viablePatternCache[pattern] = false;
+        return false;
+    }
 
-        foreach (var l in PossibleSubPatternLengths(pattern))
+    public int CountPatternOptions(string pattern) => FindPatternOptions(pattern).Count;
+    
+    public HashSet<string> FindPatternOptions(string pattern)
+    {
+        if (pattern.Length == 0 || !IsViablePattern(pattern))
         {
-            var a = pattern[..l];
-            var b = pattern[l..];
-            if (_possiblePatterns.Contains(a) && IsViablePattern(b))
-            {
-                _possiblePatterns.Add(pattern);
-                return true;
-            }
+            _patternOptionCache[pattern] = [];
+            return _patternOptionCache[pattern];
         }
 
-        return false;
+        if (_patternOptionCache.TryGetValue(pattern, out var patternCount))
+        {
+            return patternCount;
+        }
+
+        var options = PossibleSubPatternLengths(pattern)
+            .Select(l => (pattern[..l], pattern[l..]))
+            .Where(x => _patterns.Contains(x.Item1))
+            .SelectMany(x => FindPatternOptions(x.Item2)
+                .Select(y => string.Join(".", x.Item1, y)))
+            .ToHashSet();
+
+        _patternOptionCache[pattern] = options;
+        return options;
     }
 
     private IEnumerable<int> PossibleSubPatternLengths(string pattern)
     {
-        for (int i = _minLength; i < pattern.Length; i++)
+        for (int i = 1; i < pattern.Length; i++)
         {
             if (i > _maxLength)
             {
@@ -88,13 +138,25 @@ public class Day19
 
     public static int CountViablePatterns(string input)
     {
-        (var startPatterns, var targetPatterns) = Parse(input);
+        var (startPatterns, targetPatterns) = Parse(input);
         return CountViablePatterns(startPatterns, targetPatterns);
+    }
+
+    public static int CountTotalPatternOptions(string input)
+    {
+        var (startPatterns, targetPatterns) = Parse(input);
+        return CountTotalPatternOptions(startPatterns, targetPatterns);
     }
 
     private static int CountViablePatterns(IEnumerable<string> startPatterns, IEnumerable<string> targetPatterns)
     {
         var d = new Day19(startPatterns);
         return d.CountViablePatterns(targetPatterns);
+    }
+
+    private static int CountTotalPatternOptions(IEnumerable<string> startPatterns, IEnumerable<string> targetPatterns)
+    {
+        var d = new Day19(startPatterns);
+        return d.TotalPatternOptions(targetPatterns);
     }
 }
--- AdventOfCodeTests/2024/Day19Tests.cs
+++ AdventOfCodeTests/2024/Day19Tests.cs
@@ -25,6 +25,16 @@ bbrgwb";
             yield return new object[] { startPatterns, targetPatterns[i], expectedResults[i] };
         }
     }
+    
+    public static IEnumerable<object[]> TestDataCounts()
+    {
+        int[] expectedResults = [2, 1, 4, 6, 0, 1, 2, 0];
+        (var startPatterns, var targetPatterns) = Day19.Parse(TestInput);
+        for (var i = 0; i < targetPatterns.Count; i++)
+        {
+            yield return new object[] { startPatterns, targetPatterns[i], expectedResults[i] };
+        }
+    }
 
     [Theory]
     [MemberData(nameof(TestData))]
@@ -39,5 +49,20 @@ bbrgwb";
     public void TestCountViablePatterns(string input, int expectedResult)
     {
         Day19.CountViablePatterns(input).Should().Be(expectedResult);
+    }
+    
+    [Theory]
+    [MemberData(nameof(TestDataCounts))]
+    public void TestCountPatternOptions(string[] startPatterns, string targetPattern, int expectedResult)
+    {
+        var d = new Day19(startPatterns);
+        d.FindPatternOptions(targetPattern).Should().HaveCount(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(TestInput, 16)]
+    public void TestTotalPatternOptions(string input, int expectedResult)
+    {
+        Day19.CountTotalPatternOptions(input).Should().Be(expectedResult);
     }
 }
